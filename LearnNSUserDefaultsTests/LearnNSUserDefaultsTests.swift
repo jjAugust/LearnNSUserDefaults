785649f9583aca466d7383e6a269a833e02428fc
BigTortoise <302899093@qq.com>
2015-11-07 17:19:58 +0800

    Initial Commit

---
+++ LearnNSUserDefaultsTests/LearnNSUserDefaultsTests.swift
@@ -0,0 +1,36 @@
+//
+//  LearnNSUserDefaultsTests.swift
+//  LearnNSUserDefaultsTests
+//
+//  Created by 任 一杰 on 15/11/7.
+//  Copyright © 2015年 任一杰. All rights reserved.
+//
+
+import XCTest
+@testable import LearnNSUserDefaults
+
+class LearnNSUserDefaultsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
